2a94ad8c76532b69be2a236663030cdf94c2d9ee
Majdy <majdygo@gmail.com>
2020-04-11 04:00:23 +0300

    works perfect

--- staticServe.js
+++ staticServe.js
@@ -1,14 +1,11 @@
-/**
- * Created by Joe on 05/30/16.
- */
 var express =  require('express');
 
 var app = express();
-var port = 8088;
+var port = 8080;
 app.set('port', (process.env.PORT || port));
 
 app.use('/', express.static(__dirname + '/app'));
 
 app.listen(app.get('port'),function(){
-    console.log('Personetics Admin - Client running on port: ' + port);
+    console.log(' Client running on port: ' + port);
 });
